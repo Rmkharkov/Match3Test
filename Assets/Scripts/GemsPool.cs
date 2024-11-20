01d224e50dbdbda3427844b423f2f5ef2ec0f29c
Illia Hapych <illia.hapych@appsyoulove.com>
2024-11-20 02:58:53 +0200

    - init commit with base pool

---
+++ Assets/Scripts/GemsPool.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+public class GemsPool
+{
+    private readonly Dictionary<GlobalEnums.GemType, Stack<SC_Gem>> pool;
+    private SC_GameVariablesConfig GameVariables => SC_GameVariablesConfig.Instance;
+
+    public GemsPool()
+    {
+        pool = new Dictionary<GlobalEnums.GemType, Stack<SC_Gem>>();
+        for (var i = 0; i < Enum.GetNames(typeof(GlobalEnums.GemType)).Length; i++)
+        {
+            pool.TryAdd((GlobalEnums.GemType)i, new Stack<SC_Gem>(GameVariables.maxPoolSize));
+        }
+    }
+
+    public SC_Gem Get(GlobalEnums.GemType _GemType, Vector3 _Position)
+    {
+        var toReturn = pool[_GemType].Count > 0 ? pool[_GemType].Pop() : 
+            Object.Instantiate(GemPrefabByType(_GemType), _Position, Quaternion.identity);
+        toReturn.gameObject.SetActive(true);
+        return toReturn;
+    }
+
+    public void Return(SC_Gem _Gem)
+    {
+        if (pool[_Gem.type].Count < GameVariables.maxPoolSize)
+        {
+            pool[_Gem.type].Push(_Gem);
+            _Gem.gameObject.SetActive(false);
+        }
+        else
+        {
+            Object.Destroy(_Gem);
+        }
+    }
+
+    private SC_Gem GemPrefabByType(GlobalEnums.GemType _GemType)
+    {
+        switch (_GemType)
+        {
+            case GlobalEnums.GemType.bomb:
+                return GameVariables.bomb;
+            default:
+                return GameVariables.gems.FirstOrDefault(c => c.type == _GemType);
+        }
+    }
+}
---
+++ Assets/Scripts/SC_GameVariablesConfig.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "SC_GameVariablesConfig", menuName = "Configs/SC_GameVariablesConfig", order = 0)]
+public class SC_GameVariablesConfig : ScriptableObject
+{
+    public GameObject bgTilePrefabs;
+    public SC_Gem bomb;
+    public SC_Gem[] gems;
+    public float bonusAmount = 0.5f;
+    public float bombChance = 2f;
+    public int dropHeight = 0;
+    public float gemSpeed;
+    public float scoreSpeed = 5;
+    public int maxPoolSize = 100;
+    
+    [HideInInspector]
+    public int rowsSize = 7;
+    [HideInInspector]
+    public int colsSize = 7;
+        
+        #region Singleton
+        
+    public static SC_GameVariablesConfig Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = Resources.Load<SC_GameVariablesConfig>("SC_GameVariablesConfig");
+            }
+
+            return instance;
+        }
+    }
+    private static SC_GameVariablesConfig instance;
+        
+        #endregion
+}
